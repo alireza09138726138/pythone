af4225312e59b14e1f20e6c7f57f90d7da106fa2
alireza09138726138 <39461334+alireza09138726138@users.noreply.github.com>
2018-08-01 00:21:42 +0430

    Update views.py

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,13 @@
 from django.http import HttpResponse
+from django.template import loader
+
+from .models import Question
+
 
 def index(request):
-  return HttpResponse("<h1 style='color:red'>Hello, world. You're at the polls index. Alireza! <36n</h1>")
-	 
-	 
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
