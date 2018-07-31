f54afa7661a2adebc2f9883eafbd000ccd7bde1f
alireza09138726138 <39461334+alireza09138726138@users.noreply.github.com>
2018-08-01 01:04:17 +0430

    Update settings.py

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -39,14 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 ]
 
-INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
