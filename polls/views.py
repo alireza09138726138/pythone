3c3828ccb361765d26cf8d83b6ca5884310f90e1
alireza09138726138 <39461334+alireza09138726138@users.noreply.github.com>
2018-08-05 01:52:10 +0430

    Update views.py

--- polls/views.py
+++ polls/views.py
@@ -1,11 +1,5 @@
-from django.shortcuts import render_to_response,HttpResponse
+from django.http import HttpResponse
 
-def search_form(request):
-    return render_to_response('polls/index.html')
-	
-def search(request):
-    if 'q' in request.GET:
-        message = 'You searched for: %r' % request.GET['q']
-    else:
-        message = 'You submitted an empty form.'
-    return HttpResponse(message)
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
