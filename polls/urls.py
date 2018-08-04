b10d1036045a5795642cec710c5f4299f43c3ce3
alireza09138726138 <39461334+alireza09138726138@users.noreply.github.com>
2018-08-05 01:51:18 +0430

    Update urls.py

--- polls/urls.py
+++ polls/urls.py
@@ -2,9 +2,6 @@ from django.urls import path
 
 from . import views
 
-app_name = 'polls'
 urlpatterns = [
-    path('', views.search_form, name='search_form'),
-	 path('', views.search, name='search'),
-   
+    path('', views.index, name='index'),
 ]
