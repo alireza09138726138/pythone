df3d821f1b25451145ff92c4d1dd3633322f658c
alireza09138726138 <39461334+alireza09138726138@users.noreply.github.com>
2018-08-06 01:15:15 +0430

    Update admin.py

--- polls/admin.py
+++ polls/admin.py
@@ -1,10 +1,16 @@
-
-
-# Register your models here.
-
 # -*- coding: utf-8 -*-
 
 from __future__ import unicode_literals
 
+
+
 from django.contrib import admin
 
+
+from .models import Question
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fields = ['pub_date', 'question_text']
+	
+admin.site.register(Question)
