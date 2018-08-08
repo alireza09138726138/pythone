78ca3482dac3b6e732e2248266bcf3378e2704c5
alireza09138726138 <39461334+alireza09138726138@users.noreply.github.com>
2018-08-08 21:48:30 +0430

    Update admin.py

--- polls/admin.py
+++ polls/admin.py
@@ -6,5 +6,22 @@
 
 from __future__ import unicode_literals
 
+
+
+
 from django.contrib import admin
 
+from .models import Choice, Question
+
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+    
+
+class QuestionAdmin(admin.ModelAdmin):
+    # ...
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+	
+
+admin.site.register(Question, QuestionAdmin)
